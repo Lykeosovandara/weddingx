3cdbc2a4288e247bb128b392a87eabaf7a36a62b
Keo Dara <lykeosovandara@gmail.com>
2024-02-27 13:31:08 +0700

    Update appHeader.vue: Adjust background height and add Google Maps link and image

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative w-full bg-rose-gold flex h-3/4  bg-cover" :style="{ backgroundImage: `url(${backgroundUrl})` }">
+    <div class="relative w-full bg-rose-gold flex h-screen  bg-cover" :style="{ backgroundImage: `url(${backgroundUrl})` }">
         <div class=" bg-cover flex flex-col p-8 z-10">
 
             <div class="flex flex-col text-center">
@@ -17,19 +17,25 @@
         </div>
         <div class="z-10 absolute flex flex-col justify-end items-center  w-full h-full  pb-6">
             <p>
-                <svg class="w-5 h-5 fill-white mr-2 mb-1" xmlns="http://www.w3.org/2000/svg" xml:space="preserve"
+                <svg class="w-5 h-5 fill-white mr-2 mb-2" xmlns="http://www.w3.org/2000/svg" xml:space="preserve"
                     width="800" height="800" viewBox="0 0 395.71 395.71">
                     <path
                         d="M197.849 0C122.131 0 60.531 61.609 60.531 137.329c0 72.887 124.591 243.177 129.896 250.388l4.951 6.738a3.064 3.064 0 0 0 2.471 1.255 3.08 3.08 0 0 0 2.486-1.255l4.948-6.738c5.308-7.211 129.896-177.501 129.896-250.388C335.179 61.609 273.569 0 197.849 0zm0 88.138c27.13 0 49.191 22.062 49.191 49.191 0 27.115-22.062 49.191-49.191 49.191-27.114 0-49.191-22.076-49.191-49.191 0-27.129 22.076-49.191 49.191-49.191z" />
                 </svg>
 
             </p>
-            <p class="text-sm md:text-xl text-white text-center font-moulpali px-4 "> <strong>នៅគេហដ្ឋានខាងស្រី</strong> <strong> ស្ថិតនៅភូមិក្រសាំងជ័យ ឃុំបារាយណ៍
+            
+            <a href="https://www.google.com/maps">
+                <p class="text-sm md:text-xl text-white text-center font-moulpali px-4 "> <strong>នៅគេហដ្ឋានខាងស្រី</strong> <strong> ស្ថិតនៅភូមិក្រសាំងជ័យ ឃុំបារាយណ៍
                     ស្រុកបារាយណ៍
                     ខេត្តកំពង់ធំ
                     ។
                 </strong></p>
+            </a>
 
+            <a href="https://www.google.com/maps" class=" bg-white py-4 mt-4  p-40 rounded-lg flex flex-row ">
+                <img :src="map" alt="map image"  class="h-6 mr-1"> <p class=" font-light">GOOGLE MAP</p>
+            </a>
 
         </div>
 
@@ -39,6 +45,7 @@
 
 <script setup lang="ts">
 import backgroundUrl from '~/assets/images/1.1.jpg'
+import map from '~/assets/images/google-maps.png'
 import locationSvg from '~/assets/svg/location.svg'
 </script>
 
