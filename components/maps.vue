ed962757ddfeb6a4b500a4dff7c96bd2ac7503f9
Keo Dara <lykeosovandara@gmail.com>
2024-03-15 14:39:09 +0700

    Update address font size in maps.vue

--- components/maps.vue
+++ components/maps.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col p-4">
     <p class="text-xl text-pink-500 font-moulpali text-center">ទីតាំងកម្មវិធី</p>
-    <p class="text-md py-4 text-center font-kantumruy">{{ info.address }}</p>
+    <p class="text-sm   px-4 text-center text-gray-400 font-kantumruy">{{ info.address }}</p>
     <div class="h-1/2 rounded-lg">
       <iframe style="border-radius: 10px;" loading="lazy" allowfullscreen class=" w-full h-full rounded-lg"
         title="map" aria-hidden="false" tabindex="0" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDIKCYO_jZB8-MZIhV4owOBDdG_rl8Xd6M
